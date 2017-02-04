65edb36db05478f60cc99a5fa773ed3a0f00ef87
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-02-03 20:46:34 -0330

    Replace inline Motion message schema with schema from file

--- src/Hangashore/lib/drivers/wireless.ts
+++ src/Hangashore/lib/drivers/wireless.ts
@@ -7,22 +7,15 @@ export type WirelessSource = {
     rssi$: Observable<number>,
 };
 
-const protobuf = require(`protocol-buffers`);
-const messages = protobuf(`
-    ${Motion.SCHEMA}
-`);
-
 const makeWirelessDriver = (name: string) => {
     const m = WirelessLinkMicrocontroller.fromSerialPort(name);
     return (data$: Observable<Motion>) => {
-        const motion$ = data$.map(data => messages.Motion.encode({
-            surge: data.surge,
-            yaw: data.yaw,
-        }));
+        const motion$ = data$.flatMap(data =>
+            Observable.fromPromise(Motion.schema.encode(data)));
         const rssi$ = new Subject<number>();
 
         motion$.subscribe(motion => {
-            m.send(Buffer.from(motion));
+            m.send(motion);
             m.recv().then(response => rssi$.onNext(response.rssi()));
         });
         return {
