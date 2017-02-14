ea240f3b516dc8a0847bfe6f6600c20ec7a4a1e7
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-02-13 21:45:34 -0330

    Add log driver using LocalForage

---
+++ src/Hangashore/lib/drivers/log.ts
@@ -0,0 +1,25 @@
+import {kebabCase} from 'lodash';
+import {Observable} from 'rx';
+
+const LocalForage = require(`localforage`);
+
+type Config = {
+    name: string,
+    description?: string,
+};
+
+const makeLogDriver = (config: Config) => {
+    const store = LocalForage.createInstance({
+        description: config.description,
+        driver: LocalForage.INDEXEDDB,
+        name: kebabCase(config.name),
+        storeName: kebabCase(`${config.name}-kv`),
+    });
+    return (data$: Observable<any>) => {
+        data$.timestamp().subscribe(data => {
+            store.setItem(data.timestamp.toString(), data.value);
+        });
+    };
+};
+
+export {makeLogDriver};
