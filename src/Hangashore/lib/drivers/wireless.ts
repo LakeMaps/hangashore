6cce2bb9f62e0b3eeef5c787dc15ac1aa720353e
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-04-04 20:20:48 -0230

    Wait for wireless send+recv before sending, drop msgs inbetween
    Motion values are sent across the wireless link and the recv command
    is issued and awaited before sending a new transmission. Motion values
    received while waiting for one to complete are dropped on the floor.
    
    See also: Rx.Observable.prototype.flatMapLatest [1] or exhaustMap [2]
    
      [1]:https://github.com/Reactive-Extensions/RxJS/blob/v4.1.0/doc/api/core/operators/flatmaplatest.md
      [2]:http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#instance-method-exhaustMap

--- src/Hangashore/lib/drivers/wireless.ts
+++ src/Hangashore/lib/drivers/wireless.ts
@@ -15,8 +15,8 @@ const makeWirelessDriver = (name: string) => {
         const rssi$ = new Subject<number>();
 
         motion$
-            .flatMap(data => Motion.schema.encode(data))
-            .concatMap((motion) => Observable.defer(() => m.send(motion).then(_ => m.recv())))
+            .mergeMap((data) => Motion.schema.encode(data))
+            .exhaustMap((motion: Buffer) => Observable.defer(() => m.send(motion).then(_ => m.recv())))
             .subscribe((recv) => {
                 rssi$.next(recv.rssi());
             });
