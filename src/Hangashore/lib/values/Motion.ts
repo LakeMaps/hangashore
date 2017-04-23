2e6618aad669ab092e7e89db107a50073ccc85cf
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-04-22 23:52:07 -0230

    Remove unused Motion#equals function

--- src/Hangashore/lib/values/Motion.ts
+++ src/Hangashore/lib/values/Motion.ts
@@ -3,9 +3,5 @@ import {Schema} from './Schema';
 export class Motion {
     static readonly schema = Schema.of<Motion>(`Motion`);
 
-    static equals(a: Motion, b: Motion): boolean {
-        return (a.surge === b.surge && a.yaw === b.yaw);
-    }
-
     constructor(readonly surge: number, readonly yaw: number) { /* empty */ }
 }
