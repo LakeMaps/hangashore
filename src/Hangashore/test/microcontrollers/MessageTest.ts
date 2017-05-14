5c17f4ff637876ebd59c3eda78793d375909c58d
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-05-13 22:32:03 -0230

    Remove wireless link microcontroller implementation

--- src/Hangashore/test/microcontrollers/MessageTest.ts
+++
@@ -1,9 +0,0 @@
-import * as test from 'tape';
-import {Message} from '../../lib/microcontrollers';
-
-test(`Message#buffer returns correct bytes`, (t) => {
-    t.plan(1);
-
-    const message = new Message(0x10, Buffer.alloc(1));
-    t.deepEqual(message.buffer(), Buffer.from([0xAA, 0x10, 0x00, 0x79, 0x2E]));
-});
