a2e99fb05d246dca1cd6df5ff9915e18954a8e69
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-05-28 15:30:07 -0230

    Remove unnecessary check for buffer size in Gps.decode

--- src/Hangashore/lib/values/Gps.ts
+++ src/Hangashore/lib/values/Gps.ts
@@ -4,13 +4,7 @@ import {Velocity} from './Velocity';
 const {Gps: GpsProtobuf} = require(`@lakemaps/schemas`);
 
 export class Gps {
-    static MESSAGE_SIZE = 54;
-
     static decode(buf: Buffer): Gps {
-        if (buf.length > Gps.MESSAGE_SIZE) {
-            return Gps.decode(buf.slice(0, Gps.MESSAGE_SIZE));
-        }
-
         const bytes = new Uint8Array(buf.buffer, buf.byteOffset, buf.byteLength / Uint8Array.BYTES_PER_ELEMENT);
         const obj = GpsProtobuf.deserializeBinary(bytes);
         return new Gps(
