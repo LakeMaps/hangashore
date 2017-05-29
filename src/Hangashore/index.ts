9995fdb962f9b4502a276b44d1b827c585ea3e53
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-05-29 20:00:39 -0230

    Use rxjs to subscribe to app events in main process

--- src/Hangashore/index.ts
+++ src/Hangashore/index.ts
@@ -1,22 +1,25 @@
 import {app, BrowserWindow} from 'electron';
+import {Observable} from 'rxjs';
 
 let w: any;
 let isDevelopment = () => process.env.NODE_ENV === `development`;
 
-app.on(`ready`, () => {
-    w = new BrowserWindow({
-        height: 768,
-        width: 1024,
+Observable.fromEvent(app, `ready`)
+    .subscribe(() => {
+        w = new BrowserWindow({
+            height: 768,
+            width: 1024,
+        });
+        w.loadURL(`file://${__dirname}/index.html`);
+        w.on(`closed`, () => w = null);
+        if (isDevelopment()) {
+            w.webContents.openDevTools();
+        }
     });
-    w.loadURL(`file://${__dirname}/index.html`);
-    w.on(`closed`, () => w = null);
-    if (isDevelopment()) {
-        w.webContents.openDevTools();
-    }
-});
 
-app.on(`window-all-closed`, () => {
-    if (process.platform !== `darwin`) {
-        app.quit();
-    }
-});
+Observable.fromEvent(app, `window-all-closed`)
+    .subscribe(() => {
+        if (process.platform !== `darwin`) {
+            app.quit();
+        }
+    });
