dcdcd11cccfca6130206ab37994f4e24ecef440e
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-05-28 21:38:03 -0230

    Add MissionInformation value type

---
+++ src/Hangashore/lib/values/MissionInformation.ts
@@ -0,0 +1,23 @@
+const {MissionInformation: MissionInformationProtobuf, TypedMessage} = require(`@lakemaps/schemas`);
+
+export class MissionInformation {
+    static decode(buf: Buffer): MissionInformation {
+        const bytes = new Uint8Array(buf.buffer, buf.byteOffset, buf.byteLength / Uint8Array.BYTES_PER_ELEMENT);
+        const obj = TypedMessage.deserializeBinary(bytes).getMissionInformation();
+        return new MissionInformation(obj.getDistanceToWaypoint());
+    }
+
+    private message: any;
+
+    constructor(readonly distanceToWaypoint: number) {
+        const missionInformation = new MissionInformationProtobuf();
+        missionInformation.setDistanceToWaypoint(distanceToWaypoint);
+        this.message = new TypedMessage();
+        this.message.setType(TypedMessage.Type.MISSION_INFORMATION);
+        this.message.setMissionInformation(missionInformation);
+    }
+
+    encode(): Buffer {
+        return Buffer.from(this.message.serializeBinary());
+    }
+}
