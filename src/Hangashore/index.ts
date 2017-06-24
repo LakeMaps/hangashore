d16c4f7ce27182189cb53208a3627015ba0c7394
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-06-24 14:14:46 -0230

    Revert "Don't open settings window at launch"
    This reverts commit 6d3f91a285b2137e297d073b120c2131d1d0994c.

--- src/Hangashore/index.ts
+++ src/Hangashore/index.ts
@@ -23,3 +23,9 @@ Observable.fromEvent(app, `window-all-closed`)
             app.quit();
         }
     });
+
+Observable.fromEvent(app, `ready`)
+    .subscribe(() => {
+        const s = new BrowserWindow({height: 600, width: 400});
+        s.loadURL(`file://${__dirname}/windows/settings.html`);
+    });
